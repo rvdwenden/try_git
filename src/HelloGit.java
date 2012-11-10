493c8311e547c211462549bbb45dfc826a51286e
rvdwenden <rvdwenden@zonnet.nl>
2012-11-10 16:26:34 +0100

    commit via egit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -5,7 +5,8 @@ public class HelloGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Git master");
+		System.out.println("Hello Git master develop");
+		System.out.println("merge");
 	}
 
 }
